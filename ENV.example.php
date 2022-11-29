833ebf94ea06d1eab51a2187ac664e3827f067ea
Cristo Manuel Rodríguez Rodríguez <78050608+cristomrr@users.noreply.github.com>
2022-11-29 10:37:33 +0000

    Agregado un ejemplo del archivo ENV que contiene las la configuración de la base de datos y servidor de email

---
+++ ENV.example.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Clase encargada de almacenar las variables con información sensible, para no ser envíada al repositorio
+ */
+class ENV
+{
+  // EMAIL
+  public const EMAIL_ACCOUNT = "";
+  public const EMAIL_PASSWORD = "";
+
+  // DATABASE MARIADB
+  public const DATABASE_NAME = '';
+  public const DATABASE_HOST = '';
+
+  // Configuración local:
+  public const DATABASE_USER = '';
+  public const DATABASE_PASSWORD = '';
+  public const DATABASE_PORT = '';
+
+  // Configuración servidor:
+  // public const DATABASE_USER = 'cristo';
+  // public const DATABASE_PASSWORD = 'cristo@666';
+  // public const DATABASE_PORT = 3306; 
+}
